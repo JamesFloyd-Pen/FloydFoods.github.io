f55e51ffa0329372b40899126111257541c5b823
JamesFloyd-Pen <Commanderpip@gmail.com>
2021-01-15 15:02:40 -0500

    Rename and NavBar 0.1 Test

---
+++ floyd-foods-app/src/components/navbar.jsx
@@ -0,0 +1,34 @@
+import React, { Component} from 'react';
+
+class Navbar extends Component {
+    state = {  }
+    render() { 
+        console.log('NavBar -Rendered');
+        return (
+            <nav class="topNavBar topNavBar-expand-lg topNavBar-light bg-light">
+                <div class="container-fluid"> 
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <a class="navbar-brand" href="#">Floyd Floods</a>
+                    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                            </li>
+                            <li class="nav-item">
+                            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                            </li>
+                        </ul>
+        
+                    </div>
+                </div>
+            </nav>
+        );
+    }
+}
+ 
+export default Navbar;
--- floyd-foods-app/src/index.js
+++ floyd-foods-app/src/index.js
@@ -1,13 +1,18 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import NavBar from './components/navbar';
 
 
 class App extends Component {
     render() {
       return (
+        <React.Fragment>
+                  <NavBar />
+        
         <div className="App">
           <h1>Hello, React!</h1>
         </div>
+        </React.Fragment>
       )
     }
   }
